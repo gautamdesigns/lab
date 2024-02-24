dfb051736f6c6c02eb7f854029a18c3804c5397d
Gautam Kumar <146861047+gautamdesigns@users.noreply.github.com>
2024-02-24 12:51:48 +0530

    Delete 10.js

--- 10.js
+++
@@ -1,6 +0,0 @@
-function Sort_String() {
-  var inputString = document.getElementById("str").value;
-  var sortedString = inputString.toLowerCase().split("").sort().join("");
-  document.getElementById("result").innerHTML =
-    "Characters of the string in alphabetical order are: " + sortedString;
-}
